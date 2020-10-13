1ca6d325f2379e52692ad180d21d796c0488705d
yzx <852027972@qq.com>
2020-10-13 16:39:48 +0800

    geno

--- PC-DARTS/code.sh
+++ PC-DARTS/code.sh
@@ -1,19 +1,24 @@
-nohup python -u train_search.py --gpu 2 > search.out &
+# nohup python -u train_search.py --gpu 2 > search.out &
 
-echo $!
-ps -p $!
-while [ $? = 0 ]
-do
-sleep 30s
-ps -p $!
-done
+# echo $!
+# ps -p $!
+# while [ $? = 0 ]
+# do
+# sleep 30s
+# ps -p $!
+# done
 
-nohup python -u train.py --gpu 2 > train.out &
+# nohup python -u train.py --gpu 2 > train.out &
 
-echo $!
-ps -p $!
-while [ $? = 0 ]
-do
-sleep 30s
-ps -p $!
-done
+# echo $!
+# ps -p $!
+# while [ $? = 0 ]
+# do
+# sleep 30s
+# ps -p $!
+# done
+
+save=adv_nop
+
+python test_adv.py --arch $save --batch_size 64 --gpu 2  --attack PGD \
+--model_path "/home/yuezx/NAS-Benchmark/PC-DARTS/saved_models/weights.pt"
--- PDARTS/code.sh
+++ PDARTS/code.sh
@@ -1,19 +1,24 @@
-nohup python -u train_search.py --gpu 3 > search.out &
+# nohup python -u train_search.py --gpu 3 > search.out &
 
-echo $!
-ps -p $!
-while [ $? = 0 ]
-do
-sleep 30s
-ps -p $!
-done
+# echo $!
+# ps -p $!
+# while [ $? = 0 ]
+# do
+# sleep 30s
+# ps -p $!
+# done
 
-nohup python -u train_cifar.py --gpu 3 > train.out &
+# nohup python -u train_cifar.py --gpu 3 > train.out &
 
-echo $!
-ps -p $!
-while [ $? = 0 ]
-do
-sleep 30s
-ps -p $!
-done
+# echo $!
+# ps -p $!
+# while [ $? = 0 ]
+# do
+# sleep 30s
+# ps -p $!
+# done
+
+save=adv_nop
+
+python test_adv.py --arch $save --batch_size 32 --gpu 2  --attack PGD \
+--model_path "/home/yuezx/NAS-Benchmark/PDARTS/adv_nop_train/weights.pt"
