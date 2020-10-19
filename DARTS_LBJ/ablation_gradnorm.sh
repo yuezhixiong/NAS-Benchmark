50c357509c1b90405e02bfba210683012cad62cc
yzx <852027972@qq.com>
2020-10-19 16:42:50 +0800

    u

--- DARTS_LBJ/ablation_gradnorm.sh
+++ DARTS_LBJ/ablation_gradnorm.sh
@@ -1,13 +1,13 @@
 save=ablation_gradnorm
-gpu=13
+gpu=1
 
 # python train_search.py --save $save --gpu $gpu \
 # --batch_size 64 --unrolled --cutout \
 # --adv FGSM --nop --MGDA
 
-python copy_genotype.py --save $save
+# python copy_genotype.py --save $save
 
-# python train.py --arch $save --save $save --gpu $gpu --cutout --auxiliary --batch_size 96
+python train.py --arch $save --save $save --gpu $gpu --cutout --auxiliary --batch_size 60
 
 # python test_adv.py --arch $save --batch_size 128 --gpu 12  \ 
 # --model_path "fgsm_nop_max_etp_cutout_batchsize/20201012-104509/best_model.pt"
