fd307523e820badbe13113c18782d6642eb8ded4
yzx <852027972@qq.com>
2020-10-19 10:56:57 +0800

    update

--- DARTS_LBJ/ablation_mgda.sh
+++ DARTS_LBJ/ablation_mgda.sh
@@ -1,11 +1,11 @@
 save=ablation_mgda
 gpu=14
 
-python train_search.py --save $save --gpu $gpu \
---batch_size 64 --unrolled --cutout \
---adv FGSM --nop
+# python train_search.py --save $save --gpu $gpu \
+# --batch_size 64 --unrolled --cutout \
+# --adv FGSM --nop
 
-# python copy_genotype.py --save $save
+python copy_genotype.py --save $save
 
 # python train.py --arch $save --save $save --gpu $gpu --cutout --auxiliary --batch_size 96
 
