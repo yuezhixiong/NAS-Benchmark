c7b9ec469fe49d9d32a563eb6303546090acc016
yzx <yuezhixiong915@qq.com>
2020-10-22 11:01:59 +0800

    u

---
+++ DARTS_LBJ/darts_v2.sh
@@ -0,0 +1,26 @@
+save=DARTS_V2
+gpu=1
+
+# python train_search.py --save $save --gpu $gpu \
+# --batch_size 20 --unrolled --cutout \
+# --adv FGSM --nop --MGDA --constrain min
+
+# python copy_genotype.py --save $save
+
+# python train.py --arch $save --save $save --gpu $gpu --cutout --auxiliary \
+# --batch_size 96 --init_channels 12
+
+python train.py --arch $save --save $save --gpu $gpu --cutout --auxiliary \
+--batch_size 96 --init_channels 20
+
+# python test_adv.py --arch $save --batch_size 32 --gpu $gpu --auxiliary --cutout \
+# --attack PGD --init_channels 12 \
+# --model_path "/home/yuezx/NAS-Benchmark/DARTS_LBJ/adv_nop_min/auxiliary0.4_cutout16_batchsize50_channel12/best_model.pt"
+
+# python test_adv.py --arch $save --batch_size 32 --gpu $gpu --auxiliary --cutout \
+# --attack FGSM --init_channels 20 \
+# --model_path "/home/yuezx/NAS-Benchmark/DARTS_LBJ/adv_nop_min/auxiliary0.4_cutout16_batchsize50_channel20/best_model.pt"
+
+# python test_adv.py --arch $save --batch_size 18 --gpu $gpu --auxiliary --cutout \
+# --attack PGD --init_channels 36 \
+# --model_path "/home/yuezx/NAS-Benchmark/DARTS_LBJ/adv_nop_min/auxiliary0.4_cutout16_batchsize50_channel36/best_model.pt"
