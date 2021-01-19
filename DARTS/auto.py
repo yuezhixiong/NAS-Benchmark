76f35985fb38447ad809d82f12da305ff43217c4
yuezhixiong <yuezhixiong915@qq.com>
2021-01-19 15:09:00 +0800

    u

--- DARTS/auto.py
+++ DARTS/auto.py
@@ -112,8 +112,8 @@ else:
     copyfile('auto.py', os.path.join(logpath, 'auto.py'))
     adv = 'fast'
     adv_acc_values = [(0, 1)] # [(0, 1), (1, 1)] # [(0, 1)] 
-    constrain = 'abs' # min, abs
-    constrain_mins = [3, 2, 1] # [2, 3] # [1, 2, 3]
+    constrain = 'min' # min, abs
+    constrain_mins = [2, 1] # [2, 3] # [1, 2, 3]
     temperature = 'none' # GumbelA, none, A
     fxs = ['none'] # ['Sqr', 'Cub', 'Exp', 'Tan'] # none, Sqr, Cub, Exp, Tan
     nop_outer = 1
