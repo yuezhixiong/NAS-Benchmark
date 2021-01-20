f6baedf5d5c6de354c2e3b0b15f04eff75bbc74e
yuezhixiong <852027972@qq.com>
2021-01-20 16:47:45 +0800

    u

--- DARTS/train.py
+++ DARTS/train.py
@@ -148,7 +148,8 @@ def train(train_queue, model, criterion, optimizer):
 
   for step, (input, target) in enumerate(train_queue):
     input = Variable(input).cuda()
-    target = Variable(target).cuda(async=True)
+    target = Variable(target).cuda()
+
 
     optimizer.zero_grad()
     logits, logits_aux = model(input)
@@ -180,7 +181,7 @@ def infer(valid_queue, model, criterion):
 
   for step, (input, target) in enumerate(valid_queue):
     input = Variable(input, volatile=True).cuda()
-    target = Variable(target, volatile=True).cuda(async=True)
+    target = Variable(target, volatile=True).cuda()
 
     logits, _ = model(input)
     loss = criterion(logits, target)
