137944c4b4ba9505317c788c37604d138ce1881b
yuezhixiong <yuezhixiong915@qq.com>
2021-02-01 14:32:42 +0800

    add gnloss

--- DARTS/auto.py
+++ DARTS/auto.py
@@ -113,76 +113,76 @@ else:
     adv = 'fast'
     adv_acc_values = [(0, 1)] # [(0, 1), (1, 1)] # [(0, 1)] 
     constrain = 'abs' # min, abs
-    constrain_mins = [3, 2, 1] # [2, 3] # [1, 2, 3]
+    constrain_mins = [4, 3, 2, 1] # [2, 3] # [1, 2, 3]
     temperature = 'none' # GumbelA, none, A
     fxs = ['none'] # ['Sqr', 'Cub', 'Exp', 'Tan'] # none, Sqr, Cub, Exp, Tan
     nop_outer = 1
     adv_outer = 1
     mgda = 1 # 1
-    grad_norm = 'loss' # none, l2, loss+
-    nop_later = 50 # 30
-    adv_later = 50 # 30
+    grad_norms = ['l2', 'loss+', 'loss'] #'loss' # none, l2, loss+
+    nop_later = 0 # 30
+    adv_later = 0 # 30
     epoch = 50
 
     big_alpha = 0
     search = 1
-    train = 1
-    test_adv = 1
+    train = 0
+    test_adv = 0
     datasets = ['cifar10'] #, 'cifar100']
 
     for dataset in datasets:
         for adv_lambda, acc_lambda in adv_acc_values:
             for constrain_min in constrain_mins:
                 for fx in fxs:
+                    for grad_norm in grad_norms:
+                        if big_alpha:
+                            config_name = 'bigAlphaInit'
+                        else:
+                            config_name = 'randomInit'
 
-                    if big_alpha:
-                        config_name = 'bigAlphaInit'
-                    else:
-                        config_name = 'randomInit'
+                        if dataset != 'cifar10':
+                            config_name += '_' + dataset
 
-                    if dataset != 'cifar10':
-                        config_name += '_' + dataset
+                        config_name += '_inner'
+                        if adv != 'none':
+                            if adv_lambda:
+                                config_name += '_adv' + str(adv_lambda)
+                        if acc_lambda:
+                            config_name += '_acc' + str(acc_lambda)
 
-                    config_name += '_inner'
-                    if adv != 'none':
-                        if adv_lambda:
-                            config_name += '_adv' + str(adv_lambda)
-                    if acc_lambda:
-                        config_name += '_acc' + str(acc_lambda)
+                        config_name += '_outer'
+                        
+                        if adv_outer:
+                            config_name += '_adv'
+                        if nop_outer:
+                            config_name += '_nop'
+                        if mgda:
+                            config_name += '_mgda'
+                        if constrain != 'none':
+                            config_name += '_' + constrain + str(int(constrain_min*10))
+                        if temperature != 'none':
+                            config_name += '_temp' + temperature
+                        if fx != 'none':
+                            config_name += '_fx' + fx
 
-                    config_name += '_outer'
-                    
-                    if adv_outer:
-                        config_name += '_adv'
-                    if nop_outer:
-                        config_name += '_nop'
-                    if mgda:
-                        config_name += '_mgda'
-                    if constrain != 'none':
-                        config_name += '_' + constrain + str(int(constrain_min*10))
-                    if temperature != 'none':
-                        config_name += '_temp' + temperature
-                    if fx != 'none':
-                        config_name += '_fx' + fx
+                        if nop_later != 0:
+                            config_name += '_nopLater' + str(nop_later)
+                        if adv_later != 0:
+                            config_name += '_advLater' + str(adv_later)
+                        if epoch != 50:
+                            config_name += '_epoch' + str(epoch)
+                        if grad_norm != 'none':
+                            config_name += '_gn' + grad_norm
+                        
+                        # save_path = os.path.join(logpath, config_name)
 
-                    if nop_later != 0:
-                        config_name += '_nopLater' + str(nop_later)
-                    if adv_later != 0:
-                        config_name += '_advLater' + str(adv_later)
-                    if epoch != 50:
-                        config_name += '_epoch' + str(epoch)
-                    if grad_norm != 'none':
-                        config_name += '_gn' + grad_norm
-                    
-                    # save_path = os.path.join(logpath, config_name)
+                        config_dict = {'other':{'save':config_name, 'search':search, 'train':train, 'test_adv':test_adv, 'big_alpha':big_alpha, 'dataset':dataset, 'epoch':epoch},
+                                        'inner':{'adv':adv, 'adv_lambda':adv_lambda, 'acc_lambda':acc_lambda}, 
+                                        'outer':{'nop_outer':nop_outer, 'adv_outer':adv_outer, 'mgda':mgda, 'constrain':constrain, 'constrain_min':constrain_min, 
+                                                'temperature':temperature, 'fx':fx, 'nop_later':nop_later, 'adv_later':adv_later, 'grad_norm':grad_norm}}
+                        
+                        config_parser.read_dict(config_dict)
+                        config_parser.write(open(os.path.join(logpath, config_name + '.ini'), 'w'))
 
-                    config_dict = {'other':{'save':config_name, 'search':search, 'train':train, 'test_adv':test_adv, 'big_alpha':big_alpha, 'dataset':dataset, 'epoch':epoch},
-                                    'inner':{'adv':adv, 'adv_lambda':adv_lambda, 'acc_lambda':acc_lambda}, 
-                                    'outer':{'nop_outer':nop_outer, 'adv_outer':adv_outer, 'mgda':mgda, 'constrain':constrain, 'constrain_min':constrain_min, 
-                                             'temperature':temperature, 'fx':fx, 'nop_later':nop_later, 'adv_later':adv_later, 'grad_norm':grad_norm}}
-                    
-                    config_parser.read_dict(config_dict)
-                    config_parser.write(open(os.path.join(logpath, config_name + '.ini'), 'w'))
-
-                    logging.info("now running: " + config_name + ' at gpu: ' + args.gpu)
-                    run(config_parser)
+                        logging.info("now running: " + config_name + ' at gpu: ' + args.gpu)
+                        run(config_parser)
