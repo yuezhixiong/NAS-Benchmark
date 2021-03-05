fd0daac3241788949cca0e27c342ded13c1aca0f
yuezhixiong <yuezhixiong915@qq.com>
2021-03-05 13:08:47 +0800

    u

--- PDARTS/pdarts_e2rnas.sh
+++ PDARTS/pdarts_e2rnas.sh
@@ -1,23 +1,23 @@
 save=pdarts_e2rnas
 gpu=$1
 
-python train_search.py \
---save $save --gpu $gpu \
---layers 5 --add_layers 6 --add_layers 12 --dropout_rate 0.0 --dropout_rate 0.4 --dropout_rate 0.7 \
---nop_outer --adv_outer --flp_outer --ood_outer --MGDA --constrain abs --constrain_min 3 --grad_norm l2
+# python train_search.py \
+# --save $save --gpu $gpu --batch_size 48 \
+# --layers 5 --add_layers 6 --add_layers 12 --dropout_rate 0.0 --dropout_rate 0.4 --dropout_rate 0.7 \
+# --nop_outer --adv_outer --flp_outer --ood_outer --MGDA --constrain abs --constrain_min 3 --grad_norm l2
 
-python copy_genotype.py --save $save
+# python copy_genotype.py --save $save
 
-python train.py \
---save $save --dataset cifar10 --layers 20 \
---arch $save --gpu $gpu --auxiliary --cutout 
+# python train.py \
+# --save $save --dataset cifar10 --layers 20 \
+# --arch $save --gpu $gpu --auxiliary --cutout 
 
-# model="/data/yuezx/NAS-Benchmark/PDARTS/pdarts_e2rnas/cifar10_channel36/weights.pt"
-# python test_adv.py --arch $save --gpu $gpu \
-# --model_path $model
+model="pdarts_e2rnas/cifar10_channel36/weights.pt"
+python test_adv.py --arch $save --gpu $gpu \
+--model_path $model
 
-# python test_ood.py --arch $save --gpu $gpu \
-# --model_path $model
+python test_ood.py --arch $save --gpu $gpu \
+--model_path $model
 
-# python test_flp.py --arch $save --gpu $gpu \
-# --model_path $model
+python test_flp.py --arch $save --gpu $gpu \
+--model_path $model
