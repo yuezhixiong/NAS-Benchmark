834f4b3308df24123ee5b216acae06f3dfb02af3
yuezhixiong <852027972@qq.com>
2021-03-11 10:48:14 +0800

    u

--- PDARTS/PDARTS_cifar100.sh
+++ PDARTS/PDARTS_cifar100.sh
@@ -1,11 +1,16 @@
 save=PDARTS_cifar100
-gpu=2
+gpu=0
 
-# python train_cifar.py \
-# --save $save --dataset CIFAR100 --layers 20 \
-# --arch $save --gpu $gpu --auxiliary --cutout 
-# layers 20 for cifar10 and cifar100, 8 for sport8, mit67 and flowers102
+python train.py \
+--save $save --dataset cifar100 --layers 20 \
+--arch $save --gpu $gpu --auxiliary --cutout 
 
-python test_adv.py --arch $save --batch_size 32 --gpu $gpu --auxiliary --cutout \
---attack PGD --init_channels 36 --dataset cifar100 --layers 20 \
---model_path "/home/yuezx/NAS-Benchmark/PDARTS/PDARTS_cifar100/batchsize32_channel36/weights.pt"
+model="PDARTS_cifar100/cifar100_channel36/weights.pt"
+python test_adv.py --arch $save --gpu $gpu \
+--model_path $model --dataset cifar100
+
+python test_ood.py --arch $save --gpu $gpu \
+--model_path $model --dataset cifar100
+
+python test_flp.py --arch $save --gpu $gpu \
+--model_path $model
